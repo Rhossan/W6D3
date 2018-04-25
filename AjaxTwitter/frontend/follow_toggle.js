48c52bde8513b35449874b183e2d62e0f7882a3d
Rhossan <rhossan93@gmail.com>
2018-04-25 17:21:02 -0400

    fixes to constructor and added render

--- AjaxTwitter/frontend/follow_toggle.js
+++ AjaxTwitter/frontend/follow_toggle.js
@@ -8,6 +8,26 @@ class FollowToggle {
     this.followState = (this.$el.data('initial-follow-state') ||
                         options.followState);
   }
+
+  render() {
+    switch (this.followState) {
+      case 'followed':
+        this.$el.prop('disabled', false);
+        this.$el.html('Unfollow!');
+        break;
+      case 'unfollowed':
+        this.$el.prop('disabled', false);
+        this.$el.html('Follow!');
+        break;
+      case 'following':
+        this.$el.prop('disabled', true);
+        this.$el.html('Following...');
+        break;
+      case 'unfollowing':
+        this.$el.prop('disabled', true);
+        this.$el.html('Unfollowing...');
+        break;
+    }
 }
 
 
